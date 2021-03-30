b010714efa0ff6c05e8efa0324b5ba24810cd400
Andreas Kloeckner <inform@tiker.net>
2021-03-29 21:08:52 -0500

    Muck around with emacs

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -11,225 +11,6 @@
 
 (add-to-list 'load-path (concat dotfiles-dir "my-packages"))
 
-; set up various stuff -----------------------------------------------------
-(when (string= system-name "lightning")
-  (require 'notmuch)
-  (require 'org-notmuch)
-  )
-
-(require 'gnus-art)
-(require 'gnus-pers)
-(require 'zoom-frm)
-(require 'helm-config)
-(gnus-personality-init)
-(setq inhibit-splash-screen t)
-
-;(require 'atomic-chrome)
-;(atomic-chrome-start-server)
-
-; Prevent changing font on DPI change?
-; https://emacs.stackexchange.com/a/32664
-; (define-key special-event-map [config-changed-event] 'ignore)
-
-(defun my-message-setup-routine ()
-  (flyspell-mode 1)
-  (abbrev-mode 1)
-
-  ; ; https://emacs.stackexchange.com/questions/19867/set-the-pgp-signing-marker-to-the-top-of-the-new-message/19869
-  ; (goto-char (point-min))
-  ; (search-forward "--text follows this line--")
-  ; (end-of-line)
-  ; (insert "\n")
-
-  ; (mml-secure-message-sign)
-  )
-
-(add-hook 'message-setup-hook 'my-message-setup-routine)
-
-; {{{ MML SIGN FIX BEGIN
-
-; https://svn.red-bean.com/repos/kfogel/trunk/.emacs:
-
-;; Anyway, the main thing is, when this wasn't set, then sending a
-;; GPG-signed message (which happens automatically when replying to
-;; someone else's GPG-signed message) would error with the familiar
-;; "Couldn't find any signer names." message, the same one that, IIRC,
-;; prompted me to set `mml-secure-smime-sign-with-sender' earlier.
-;; Please, please let this work for at least a few years this time.
-;;
-;; Aha!  It's all https://debbugs.gnu.org/cgi/bugreport.cgi?bug=40118
-;; Thanks to Eli Zaretskii and Robert Pluim for pointing this out.
-
-;(setq mml-secure-openpgp-sign-with-sender t)
-;(setq mml-secure-smime-sign-with-sender t)
-(setq mm-sign-option 'guided)
-
-; }}}
-
-
-(ispell-change-dictionary "american")
-
-(global-flycheck-mode)
-
-(defun fd-switch-dictionary()
-  (interactive)
-  (let* ((dic ispell-current-dictionary)
-         (change (if (string= dic "de_DE") "american" "de_DE")))
-    (ispell-change-dictionary change)
-    (message "Dictionary switched from %s to %s" dic change)
-    ))
-
-(global-set-key (kbd "<f7>")   'fd-switch-dictionary)
-(setq evil-want-C-i-jump nil)
-(setq evil-want-C-u-scroll t)
-
-; needed by evil-collection
-; (setq evil-want-keybinding nil)
-
-(require 'evil)
-(evil-mode 1)
-(evil-set-initial-state 'notmuch-tree-mode 'emacs)
-; (evil-collection-init)
-
-(require 'evil-org)
-(add-hook 'org-mode-hook 'evil-org-mode)
-(evil-org-set-key-theme '(navigation insert textobjects additional))
-(setq org-latex-prefer-user-labels t)
-
-
-(quietly-read-abbrev-file)
-(setq default-abbrev-mode t)
-
-(add-hook 'after-init-hook 'global-company-mode)
-
-(global-set-key (kbd "M-x") #'helm-M-x)
-(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
-(global-set-key (kbd "C-x C-f") #'helm-find-files)
-(helm-mode 1)
-
-; {{{ theme
-
-(defvar ak-theme-lightness 'light)
-(defun ak-toggle-theme-lightness()
-  (interactive)
-  (setq ak-theme-lightness
-        (if (eq ak-theme-lightness 'light) 'dark 'light) )
-  ;(customize-set-variable 'frame-background-mode ak-theme-lightness)
-  ; https://www.reddit.com/r/emacs/comments/30b67j/how_can_you_reset_emacs_to_the_default_theme/
-  (mapcar #'disable-theme custom-enabled-themes)
-  (if (eq ak-theme-lightness 'light)
-    (load-theme 'leuven t)
-    (progn
-      (load-theme 'zenburn t)
-      (custom-theme-set-faces
-        'zenburn
-        '(org-level-1 ((t (:background "#666666" :height 1.3 :extend t))))
-        '(org-level-2 ((t (:background "#555555" :height 1.15 :extend t))))
-        '(org-level-3 ((t (:background "#444444" :height 1.15 :extend t))))
-        )
-      ; https://emacs.stackexchange.com/a/60628
-      (enable-theme 'zenburn)
-      )
-    )
-  )
-
-; }}}
-
-(setq-default indent-tabs-mode nil)
-
-(setq org-fontify-whole-heading-line t)
-
-; https://emacs.stackexchange.com/questions/38184/org-mode-ignore-heading-when-exporting-to-latex
-(require 'ox-extra)
-(ox-extras-activate '(ignore-headlines))
-
-(evilem-default-keybindings "C-m") 
-
-; (server-mode 1)
-; (global-auto-revert-mode t)
-
-; {{{ sync-org-to-git 
-
-(defun sync-org-to-git ()
-  (interactive)
-  (let (bufs-to-delete
-        (orgdir (expand-file-name "~/org/")))
-    (dolist (buf (buffer-list) bufs-to-delete)
-      (let (fn dn)
-        (setq fn (buffer-file-name buf))
-        (setq dn
-              (if fn
-                  (file-name-directory fn)
-                nil))
-        (if (equal dn orgdir)
-            (setq bufs-to-delete (cons buf bufs-to-delete)))
-        ))
-    (dolist (buf bufs-to-delete)
-      (with-current-buffer buf (save-buffer))
-      (kill-buffer buf)
-      )
-    (shell-command "cd ~/org && git sync &")
-    ))
-
-; }}}
-
-; {{{ disable doc-view-mode
-
-; https://emacs.stackexchange.com/questions/22736/org-mode-how-to-disable-automatic-doc-view-on-emacs-25
-(defun ensc/mailcap-mime-data-filter (filter)
-  ""
-  (mapcar (lambda(major)
-        (append (list (car major))
-            (remove nil
-                (mapcar (lambda(minor)
-                      (when (funcall filter (car major) (car minor) (cdr minor))
-                    minor))
-                    (cdr major)))))
-      mailcap-mime-data))
-
-(defun ensc/no-pdf-doc-view-filter (major minor spec)
-  (if (and (string= major "application")
-       (string= minor "pdf")
-       (member '(viewer . doc-view-mode) spec))
-      nil
-    t))
-
-(eval-after-load 'mailcap
-  '(progn
-     (setq mailcap-mime-data
-       (ensc/mailcap-mime-data-filter 'ensc/no-pdf-doc-view-filter))))
-
-; }}}
-
-; {{{ key bindings
-
-(define-key global-map [f9]     'compile)
-(define-key global-map [f8]     'ak-toggle-theme-lightness)
-(define-key global-map [f3]     'org-cycle-agenda-files)
-(define-key global-map [f2]     'sync-org-to-git)
-(define-key global-map [f12]     'browse-url-at-point)
-(define-key global-map [(control ?')]     'org-cycle-agenda-files)
-(define-key global-map [(control ?.)]     'browse-url)
-
-(define-key ctl-x-map [(control ?+)] 'zoom-in/out)
-(define-key ctl-x-map [(control ?-)] 'zoom-in/out)
-(define-key ctl-x-map [(control ?=)] 'zoom-in/out)
-(define-key ctl-x-map [(control ?0)] 'zoom-in/out)
-
-(define-key global-map (kbd "C-w") 'evil-window-map)
-
-;(define-key viper-vi-global-user-map "v" 'set-mark-command)
-;(define-key viper-vi-global-user-map "V" 'set-mark-command)
-
-;; '(message-default-mail-headers "Fcc: ~/mail/offlineimap-weasel/Sent")
-
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-cb" 'org-iswitchb)
-
-; }}}
-
 ; {{{ customize stuff
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -393,12 +174,12 @@ SCHEDULED: %t DEADLINE: %t
  '(org-loop-over-headlines-in-active-region t)
  '(org-preview-latex-default-process 'imagemagick)
  '(package-archives
-   '(("gnu" . "http://elpa.gnu.org/packages/")
+   '(("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa" . "https://stable.melpa.org/packages/")
      ("org" . "https://orgmode.org/elpa/")))
  '(package-enable-at-startup nil)
  '(package-selected-packages
-   '(evil-collection flycheck-pycheckers markdown-mode flycheck editorconfig magit monokai-theme zenburn-theme nyx-theme dracula-theme nord-theme gruvbox-theme org-plus-contrib olivetti leuven-theme focus org-ref evil-easymotion base16-theme htmlize ox-gfm org helm zoom-frm color-theme-solarized evil evil-org company))
+   '(evil-collection flycheck-pycheckers markdown-mode flycheck editorconfig magit monokai-theme zenburn-theme nyx-theme dracula-theme nord-theme gruvbox-theme org-plus-contrib olivetti leuven-theme focus org-ref evil-easymotion base16-theme htmlize ox-gfm org helm color-theme-solarized evil evil-org company))
  '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
  '(printer-name "PDF")
  '(ps-paper-type 'letter)
@@ -461,6 +242,225 @@ SCHEDULED: %t DEADLINE: %t
  '(notmuch-search-flagged-face ((t (:foreground "yellow"))))
  '(notmuch-tag-flagged ((t (:foreground "yellow"))))
  '(org-todo ((t (:background "#dc322f" :foreground "#fff" :weight bold)))))
+
+; }}}
+
+; set up various stuff -----------------------------------------------------
+(when (string= system-name "lightning")
+  (require 'notmuch)
+  (require 'org-notmuch)
+  )
+
+(require 'gnus-art)
+(require 'gnus-pers)
+(require 'zoom-frm)
+(require 'helm-config)
+(gnus-personality-init)
+(setq inhibit-splash-screen t)
+
+;(require 'atomic-chrome)
+;(atomic-chrome-start-server)
+
+; Prevent changing font on DPI change?
+; https://emacs.stackexchange.com/a/32664
+; (define-key special-event-map [config-changed-event] 'ignore)
+
+(defun my-message-setup-routine ()
+  (flyspell-mode 1)
+  (abbrev-mode 1)
+
+  ; ; https://emacs.stackexchange.com/questions/19867/set-the-pgp-signing-marker-to-the-top-of-the-new-message/19869
+  ; (goto-char (point-min))
+  ; (search-forward "--text follows this line--")
+  ; (end-of-line)
+  ; (insert "\n")
+
+  ; (mml-secure-message-sign)
+  )
+
+(add-hook 'message-setup-hook 'my-message-setup-routine)
+
+; {{{ MML SIGN FIX BEGIN
+
+; https://svn.red-bean.com/repos/kfogel/trunk/.emacs:
+
+;; Anyway, the main thing is, when this wasn't set, then sending a
+;; GPG-signed message (which happens automatically when replying to
+;; someone else's GPG-signed message) would error with the familiar
+;; "Couldn't find any signer names." message, the same one that, IIRC,
+;; prompted me to set `mml-secure-smime-sign-with-sender' earlier.
+;; Please, please let this work for at least a few years this time.
+;;
+;; Aha!  It's all https://debbugs.gnu.org/cgi/bugreport.cgi?bug=40118
+;; Thanks to Eli Zaretskii and Robert Pluim for pointing this out.
+
+;(setq mml-secure-openpgp-sign-with-sender t)
+;(setq mml-secure-smime-sign-with-sender t)
+(setq mm-sign-option 'guided)
+
+; }}}
+
+
+(ispell-change-dictionary "american")
+
+(global-flycheck-mode)
+
+(defun fd-switch-dictionary()
+  (interactive)
+  (let* ((dic ispell-current-dictionary)
+         (change (if (string= dic "de_DE") "american" "de_DE")))
+    (ispell-change-dictionary change)
+    (message "Dictionary switched from %s to %s" dic change)
+    ))
+
+(global-set-key (kbd "<f7>")   'fd-switch-dictionary)
+(setq evil-want-C-i-jump nil)
+(setq evil-want-C-u-scroll t)
+
+; needed by evil-collection
+; (setq evil-want-keybinding nil)
+
+(require 'evil)
+(evil-mode 1)
+(evil-set-initial-state 'notmuch-tree-mode 'emacs)
+; (evil-collection-init)
+
+(require 'evil-org)
+(add-hook 'org-mode-hook 'evil-org-mode)
+(evil-org-set-key-theme '(navigation insert textobjects additional))
+(setq org-latex-prefer-user-labels t)
+
+
+(quietly-read-abbrev-file)
+(setq default-abbrev-mode t)
+
+(add-hook 'after-init-hook 'global-company-mode)
+
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode 1)
+
+; {{{ theme
+
+(defvar ak-theme-lightness 'light)
+(defun ak-toggle-theme-lightness()
+  (interactive)
+  (setq ak-theme-lightness
+        (if (eq ak-theme-lightness 'light) 'dark 'light) )
+  ;(customize-set-variable 'frame-background-mode ak-theme-lightness)
+  ; https://www.reddit.com/r/emacs/comments/30b67j/how_can_you_reset_emacs_to_the_default_theme/
+  (mapcar #'disable-theme custom-enabled-themes)
+  (if (eq ak-theme-lightness 'light)
+    (load-theme 'leuven t)
+    (progn
+      (load-theme 'zenburn t)
+      (custom-theme-set-faces
+        'zenburn
+        '(org-level-1 ((t (:background "#666666" :height 1.3 :extend t))))
+        '(org-level-2 ((t (:background "#555555" :height 1.15 :extend t))))
+        '(org-level-3 ((t (:background "#444444" :height 1.15 :extend t))))
+        )
+      ; https://emacs.stackexchange.com/a/60628
+      (enable-theme 'zenburn)
+      )
+    )
+  )
+
+; }}}
+
+(setq-default indent-tabs-mode nil)
+
+(setq org-fontify-whole-heading-line t)
+
+; https://emacs.stackexchange.com/questions/38184/org-mode-ignore-heading-when-exporting-to-latex
+(require 'ox-extra)
+(ox-extras-activate '(ignore-headlines))
+
+; (evilem-default-keybindings "C-m") 
+
+; (server-mode 1)
+; (global-auto-revert-mode t)
+
+; {{{ sync-org-to-git 
+
+(defun sync-org-to-git ()
+  (interactive)
+  (let (bufs-to-delete
+        (orgdir (expand-file-name "~/org/")))
+    (dolist (buf (buffer-list) bufs-to-delete)
+      (let (fn dn)
+        (setq fn (buffer-file-name buf))
+        (setq dn
+              (if fn
+                  (file-name-directory fn)
+                nil))
+        (if (equal dn orgdir)
+            (setq bufs-to-delete (cons buf bufs-to-delete)))
+        ))
+    (dolist (buf bufs-to-delete)
+      (with-current-buffer buf (save-buffer))
+      (kill-buffer buf)
+      )
+    (shell-command "cd ~/org && git sync &")
+    ))
+
+; }}}
+
+; {{{ disable doc-view-mode
+
+; https://emacs.stackexchange.com/questions/22736/org-mode-how-to-disable-automatic-doc-view-on-emacs-25
+(defun ensc/mailcap-mime-data-filter (filter)
+  ""
+  (mapcar (lambda(major)
+        (append (list (car major))
+            (remove nil
+                (mapcar (lambda(minor)
+                      (when (funcall filter (car major) (car minor) (cdr minor))
+                    minor))
+                    (cdr major)))))
+      mailcap-mime-data))
+
+(defun ensc/no-pdf-doc-view-filter (major minor spec)
+  (if (and (string= major "application")
+       (string= minor "pdf")
+       (member '(viewer . doc-view-mode) spec))
+      nil
+    t))
+
+(eval-after-load 'mailcap
+  '(progn
+     (setq mailcap-mime-data
+       (ensc/mailcap-mime-data-filter 'ensc/no-pdf-doc-view-filter))))
+
+; }}}
+
+; {{{ key bindings
+
+(define-key global-map [f9]     'compile)
+(define-key global-map [f8]     'ak-toggle-theme-lightness)
+(define-key global-map [f3]     'org-cycle-agenda-files)
+(define-key global-map [f2]     'sync-org-to-git)
+(define-key global-map [f12]     'browse-url-at-point)
+(define-key global-map [(control ?')]     'org-cycle-agenda-files)
+(define-key global-map [(control ?.)]     'browse-url)
+
+(define-key ctl-x-map [(control ?+)] 'zoom-in/out)
+(define-key ctl-x-map [(control ?-)] 'zoom-in/out)
+(define-key ctl-x-map [(control ?=)] 'zoom-in/out)
+(define-key ctl-x-map [(control ?0)] 'zoom-in/out)
+
+(define-key global-map (kbd "C-w") 'evil-window-map)
+
+;(define-key viper-vi-global-user-map "v" 'set-mark-command)
+;(define-key viper-vi-global-user-map "V" 'set-mark-command)
+
+;; '(message-default-mail-headers "Fcc: ~/mail/offlineimap-weasel/Sent")
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
 
 ; }}}
 
