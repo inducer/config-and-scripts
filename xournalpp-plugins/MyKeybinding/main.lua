c662fa94478a75ca4daed2b1ea8fc78dd75658b0
Andreas Kloeckner <inform@tiker.net>
2021-04-06 09:33:46 -0500

    Tweak xournalpp keyboard shortcuts

--- xournalpp-plugins/MyKeybinding/main.lua
+++ xournalpp-plugins/MyKeybinding/main.lua
@@ -1,9 +1,11 @@
+-- https://xournalpp.github.io/guide/plugins/plugins/
+-- 
 function initUi()
-  print("Hello from Andreas' example plugin\n");
-
-  ref = app.registerUi({["menu"] = "Select Pen tool", ["callback"] = "select_pen", ["accelerator"] = "1"});
-  ref = app.registerUi({["menu"] = "Select Highlighter tool", ["callback"] = "select_highlighter", ["accelerator"] = "2"});
-  ref = app.registerUi({["menu"] = "Select Text tool", ["callback"] = "select_text", ["accelerator"] = "3"});
+  -- https://github.com/xournalpp/xournalpp/blob/master/src/control/Control.cpp#L368
+  ref = app.registerUi({["menu"] = "Select Pen tool", ["callback"] = "select_pen", ["accelerator"] = "<Control>1"});
+  ref = app.registerUi({["menu"] = "Select Highlighter tool", ["callback"] = "select_highlighter", ["accelerator"] = "<Control>2"});
+  ref = app.registerUi({["menu"] = "Select Text tool", ["callback"] = "select_text", ["accelerator"] = "<Control>3"});
+  ref = app.registerUi({["menu"] = "Select rectangle selection tool", ["callback"] = "select_rect_sel", ["accelerator"] = "<Control>4"});
 end
 
 -- Callback if the menu item is executed
@@ -15,4 +17,7 @@ function select_highlighter()
 end
 function select_text()
     app.uiAction({["action"]="ACTION_TOOL_TEXT"})
+end
+function select_rect_sel()
+    app.uiAction({["action"]="ACTION_TOOL_SELECT_RECT"})
 end
