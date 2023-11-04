9fcff94704f3cd3699c3777cde3e9a35f69d7dff
Andreas Kloeckner <inform@tiker.net>
2023-11-04 17:49:06 -0500

    Configure evil undo system

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -31,6 +31,7 @@
  '(company-quickhelp-color-background "#4F4F4F")
  '(company-quickhelp-color-foreground "#DCDCCC")
  '(delete-by-moving-to-trash t)
+ '(evil-undo-system 'undo-redo)
  '(fci-rule-color "#383838")
  '(global-auto-revert-mode t)
  '(gnus-alias-default-identity "uofi")
