dbb36fb18e70828a399521dc12685bc78cb83837
Andreas Kloeckner <inform@tiker.net>
2024-01-24 16:23:09 -0600

    Update from emacs package update

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -167,7 +167,7 @@
      ("melpa-stable" . "https://stable.melpa.org/packages/")))
  '(package-enable-at-startup nil)
  '(package-selected-packages
-   '(org-contrib magit-delta ace-window evil-collection flycheck-pycheckers markdown-mode flycheck editorconfig magit monokai-theme zenburn-theme nyx-theme dracula-theme nord-theme gruvbox-theme olivetti leuven-theme focus evil-easymotion base16-theme htmlize ox-gfm org helm color-theme-solarized evil evil-org company))
+   '(notmuch org-contrib magit-delta ace-window evil-collection flycheck-pycheckers markdown-mode flycheck editorconfig magit monokai-theme zenburn-theme nyx-theme dracula-theme nord-theme gruvbox-theme olivetti leuven-theme focus evil-easymotion base16-theme htmlize ox-gfm org helm color-theme-solarized evil evil-org company))
  '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
  '(printer-name "PDF")
  '(ps-paper-type 'letter)
