a85e05a365c18745b52511e87b7d6ffd6dd6835f
Andreas Kloeckner <inform@tiker.net>
2025-02-20 14:51:18 -0600

    More OpenWRT base setup

--- base-install-openwrt.sh
+++ base-install-openwrt.sh
@@ -1,9 +1,17 @@
-opkg update
-opkg install \
-  nut nut-common nut-driver-apcupsd-ups nut-server nut-upsc nut-upscmd \
-  luci-app-nut \
-  kmod-usb3 \
-  htop tmux \
-  luci-app-ddns luci-app-upnp \
+#! /bin/bash
+
+PACKAGES=(
+  apcupsd
+  kmod-usb3 kmod-usb-storage kmod-fs-ext4
+  # for apcupsd
+  kmod-usb-hid
+
+  htop tmux
+  luci-app-ddns luci-app-upnp
   etherwake luasocket
+  msmtp-mta
+)
+
+opkg update
+opkg install "${PACKAGES[@]}"
 
