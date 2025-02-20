3178f125e1668cecdf93dc82c2d87dcef3c8c744
Andreas Kloeckner <inform@tiker.net>
2025-02-20 09:26:09 -0600

    Add base-install-openwrt

---
+++ base-install-openwrt.sh
@@ -0,0 +1,9 @@
+opkg update
+opkg install \
+  nut nut-common nut-driver-apcupsd-ups nut-server nut-upsc nut-upscmd \
+  luci-app-nut \
+  kmod-usb3 \
+  htop tmux \
+  luci-app-ddns luci-app-upnp \
+  etherwake luasocket
+
