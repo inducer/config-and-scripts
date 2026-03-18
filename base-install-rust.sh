873d8e8f52ff77111f8e2eea4bc8cd7bd254b6e2
Andreas Kloeckner <inform@tiker.net>
2026-03-18 18:27:40 -0500

    base-install-rust: add tectonic

--- base-install-rust.sh
+++ base-install-rust.sh
@@ -18,5 +18,9 @@ ci difftastic
 ci --git https://codeberg.org/mergiraf/mergiraf.git
 ci yazi-build
 
+# https://tectonic-typesetting.github.io/book/latest/howto/build-tectonic/external-dep-install.html
+CXXFLAGS="-std=c++20" ci tectonic -F external-harfbuzz
+(cd "$HOME/.cargo/bin" && ln -s tectonic nextonic)
+
 ci --git https://github.com/latex-lsp/texlab
 # --tag <insert version here>
