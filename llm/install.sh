5a6e8ebb4be10947584ff5680ea69fadb6355a1c
Andreas Kloeckner <inform@tiker.net>
2026-05-07 13:53:49 -0500

    LLM: Install vllm audio capabilities

--- llm/install.sh
+++ llm/install.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
+set -euo pipefail
+
 uv venv --python 3.12 --seed
 source .venv/bin/activate
-uv pip install vllm --torch-backend=auto
-# git clone https://github.com/vllm-project/vllm.git vllm-git --depth 1
+uv pip install vllm[audio] --torch-backend=auto
+git clone https://github.com/vllm-project/vllm.git vllm-git --depth 1
