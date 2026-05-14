b3c2be6952188209bc9f923467c698c8ff1cc7e8
Andreas Kloeckner <inform@tiker.net>
2026-05-14 15:51:16 -0500

    Minor LLM install tweaks

--- llm/install.sh
+++ llm/install.sh
@@ -2,7 +2,20 @@
 
 set -euo pipefail
 
+rm -Rf ~/vllm/.venv
+cd ~/vllm
 uv venv --python 3.12 --seed
 source .venv/bin/activate
+
+cd ~/pack
+git clone https://github.com/vllm-project/vllm.git --depth 1 || true
+cd vllm
+
+git checkout -f
+
+# # https://github.com/vllm-project/vllm/pull/31607
+# curl https://patch-diff.githubusercontent.com/raw/vllm-project/vllm/pull/31607.diff  | patch -p1
+
+# uv pip install .[audio] --torch-backend=auto
+
 uv pip install vllm[audio] --torch-backend=auto
-git clone https://github.com/vllm-project/vllm.git vllm-git --depth 1
