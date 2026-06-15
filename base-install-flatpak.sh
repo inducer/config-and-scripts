efa498203f6f2a426d507b002bdaed7ac4c51751
Andreas Kloeckner <inform@tiker.net>
2026-06-15 16:35:42 -0500

    Add captions-to-exif

--- base-install-flatpak.sh
+++ base-install-flatpak.sh
@@ -5,7 +5,7 @@ flatpak install com.github.flxzt.rnote
 flatpak install com.spotify.Client
 flatpak install com.github.tchx84.Flatseal
 flatpak install org.freecadweb.FreeCAD
-flatpak install no.mifi.losslesscut
+# flatpak install no.mifi.losslesscut
 flatpak install com.bitwarden.desktop
 flatpak install flathub rocks.koreader.KOReader
 flatpak install flathub io.github.kolunmi.Bazaar
